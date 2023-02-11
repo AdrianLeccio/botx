982c27cd3476f7255420eb812d5af86bd710c104
AdrianLeccio <adrianlbofficial@gmail.com>
2023-02-11 14:00:33 -0300

    Update myConfig.js

--- myConfig.js
+++ myConfig.js
@@ -1,10 +1,10 @@
 let MensajeAd =
 	"​🚨*Promociona lo que desees!*🚨 \n \n" +
 	"	🎁	- Web de afiliados / referidos \n" +
-	"	🤑	- Tu cuenta de OnlyFans \n" +
-	"	📈	- Tu canal XXX \n" +
-	"	🗺	- Redirige el tráfico a donde prefieras \n \n" +
-	"['Click Aquí🔥'](https://t.me/himradrian)"
+	"	🤑	- Acortador con AdsTerra \n" +
+	"	📈	- Página con exoclick \n" +
+	"	🗺	- Redirige el tráfico a @otrosCanales o a una página web \n \n" +
+	"['Click Aquí🔥'](https://t.me/hiMrAdrian)"
 
 /*let MensajeAd3 =
 	"​🚨*Promociona lo que desees!*🚨 \n \n"*/
@@ -16,7 +16,7 @@ const N_VIDEOS = 10 //MAX 10
 
 const MY_AD = {
 	isActive: true,
-	type: 2, // //defaultAd => 1 //captionAd => 2 //linksGroupAd => 3
+	type: 3, // //defaultAd => 1 //captionAd => 2 //linksGroupAd => 3
 	data: {
 		ctn: MensajeAd,
 		image: imagenAd,
